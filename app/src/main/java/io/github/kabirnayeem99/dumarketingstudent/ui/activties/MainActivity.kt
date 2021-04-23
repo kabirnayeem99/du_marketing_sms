542ac34694c20c7527259c6584fd9ed5ea0ece1e
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-04-23 14:57:04 +0600

    📦 feat (setup): set up bottom navigation

--- app/src/main/java/io/github/kabirnayeem99/dumarketingstudent/ui/activties/MainActivity.kt
+++ app/src/main/java/io/github/kabirnayeem99/dumarketingstudent/ui/activties/MainActivity.kt
@@ -2,6 +2,10 @@ package io.github.kabirnayeem99.dumarketingstudent.ui.activties
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import io.github.kabirnayeem99.dumarketingstudent.R
 import io.github.kabirnayeem99.dumarketingstudent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +17,17 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNavBar)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fNavHost) as NavHostFragment
+        NavigationUI.setupWithNavController(bottomNavBar, navHostFragment.navController)
+
+        setUpBottomNavBar()
+
+    }
+
+    private fun setUpBottomNavBar() {
     }
 }
