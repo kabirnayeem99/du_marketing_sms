b4e36710fc60021bbf12049c452f25e55ed7fc5e
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-04-30 15:23:48 +0600

    🐛 fix: fix bottom nav ui not having animation

--- app/src/main/java/io/github/kabirnayeem99/dumarketingstudent/ui/activities/MainActivity.kt
+++ app/src/main/java/io/github/kabirnayeem99/dumarketingstudent/ui/activities/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -31,8 +30,6 @@ class MainActivity : AppCompatActivity() {
 
         setUpBottomNavBar()
         setUpNavigationDrawer()
-
-
     }
 
 
@@ -42,44 +39,9 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fNavHost) as NavHostFragment
 
-        val navController = navHostFragment.navController
-
-
-        val options = NavOptions.Builder()
-            .setLaunchSingleTop(true)
-            .setEnterAnim(R.anim.enter)
-            .setExitAnim(R.anim.exit)
-            .setPopEnterAnim(R.anim.enter)
-            .setPopExitAnim(R.anim.exit)
-            .setPopUpTo(navController.graph.startDestination, false)
-            .build()
-
-
-        bottomNavBar.setOnNavigationItemSelectedListener { menu_item ->
-            when (menu_item.itemId) {
-                R.id.homeFragment -> {
-                    navController.navigate(R.id.homeFragment, null, options)
-                }
-                R.id.noticeFragment -> {
-                    navController.navigate(R.id.noticeFragment, null, options)
-                }
-                R.id.galleryFragment -> {
-                    navController.navigate(R.id.galleryFragment, null, options)
-                }
-                R.id.facultyFragment -> {
-                    navController.navigate(R.id.facultyFragment, null, options)
-                }
-                R.id.aboutFragment -> {
-                    navController.navigate(R.id.aboutFragment, null, options)
-                }
-            }
-            true
-        }
-
-        bottomNavBar.setOnNavigationItemReselectedListener { return@setOnNavigationItemReselectedListener }
-
-//        NavigationUI.setupWithNavController(bottomNavBar, navHostFragment.navController)
+        NavigationUI.setupWithNavController(bottomNavBar, navHostFragment.navController)
     }
+
 
     private fun setUpNavigationDrawer() {
         val navigationView = binding.navDrawerView
