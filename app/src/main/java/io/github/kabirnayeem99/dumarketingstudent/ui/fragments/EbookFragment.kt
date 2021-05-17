45da3e4eb218dbede4220bb0af849d05bdd4ef9f
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-05-17 15:40:15 +0600

    📦 feat: implement downloading of ebooks

--- app/src/main/java/io/github/kabirnayeem99/dumarketingstudent/ui/fragments/EbookFragment.kt
+++ app/src/main/java/io/github/kabirnayeem99/dumarketingstudent/ui/fragments/EbookFragment.kt
@@ -1,14 +1,20 @@
 package io.github.kabirnayeem99.dumarketingstudent.ui.fragments
 
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.github.kabirnayeem99.dumarketingstudent.data.repositories.EbookRepository
+import io.github.kabirnayeem99.dumarketingstudent.data.vo.EbookData
 import io.github.kabirnayeem99.dumarketingstudent.databinding.FragmentEbookBinding
 import io.github.kabirnayeem99.dumarketingstudent.util.Resource
 import io.github.kabirnayeem99.dumarketingstudent.util.adapters.EbookDataAdapter
@@ -39,8 +45,8 @@ class EbookFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val ebookAdapter by lazy {
-            EbookDataAdapter {
-                showSnackBar("clicked ${it.title}")
+            EbookDataAdapter { ebookData ->
+                downloadEbook(ebookData)
             }
         }
 
@@ -61,6 +67,32 @@ class EbookFragment : Fragment() {
             }
 
         })
+    }
+
+    private fun downloadEbook(ebookData: EbookData) {
+
+
+        try {
+            val url = ebookData.pdfUrl
+            val request = DownloadManager.Request(Uri.parse(url)).apply {
+                setDescription("downloading ebook.")
+                setTitle("Ebook: " + ebookData.title)
+                allowScanningByMediaScanner()
+                setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                setDestinationInExternalPublicDir(
+                    Environment.DIRECTORY_DOWNLOADS,
+                    "${ebookData.title}_${ebookData.key}.pdf"
+                )
+            }
+
+            val manager: DownloadManager =
+                requireContext().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            manager.enqueue(request)
+        } catch (e: Exception) {
+            Log.e(TAG, "downloadEbook: $e")
+            Toast.makeText(context, "Could not download ${ebookData.title}", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 
 
