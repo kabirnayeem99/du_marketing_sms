efad3f362c2c4d61f9296358cb99153ee9c2a369
Naimul Kabir <kabirnayeem.99@gmail.com>
2021-07-12 20:35:39 +0600

    📦 feat: add admin app to the same repo

--- app/src/main/java/io/github/kabirnayeem99/dumarketingstudent/util/Config.kt
+++
@@ -1,7 +0,0 @@
-package io.github.kabirnayeem99.dumarketingstudent.util
-
-object Config {
-    const val SHARED_PREFERENCES_NAME =
-        "com.hellohasan.sharedpreferences_2.my_shared_preferences"
-    const val BATCH_YEAR = "batch_year"
-}
--- app/src/main/java/io/github/kabirnayeem99/dumarketingstudent/util/enums/BatchYear.kt
+++
@@ -1,9 +0,0 @@
-package io.github.kabirnayeem99.dumarketingstudent.util.enums
-
-enum class BatchYear(val value: String) {
-    First("1"),
-    Second("2"),
-    Third("3"),
-    Fourth("4"),
-    Null("0"),
-}
