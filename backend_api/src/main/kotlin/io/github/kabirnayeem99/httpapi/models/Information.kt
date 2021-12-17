48a9e760234fa9404f13e0625dd6481a7491189f
Naimul Kabir <aaron@hire-programmers.com>
2021-12-17 12:10:01 +0600

    📦 feat: make information data working as post and as get

--- backend_api/src/main/kotlin/io/github/kabirnayeem99/httpapi/models/Information.kt
+++ backend_api/src/main/kotlin/io/github/kabirnayeem99/httpapi/models/Information.kt
@@ -11,4 +11,15 @@ data class Information(
     var long: Double = 0.0,
 )
 
-val informationStorage = mutableListOf<Information>(Information("Intro", "email", "telephone", 1829.00, 2920.00))
+var information: Information = Information(
+    intro = "The Department of Marketing, one of the members of the Faculty " +
+            "of Business Studies, was created on 1st July, 1974. " +
+            "This department was established with a view to fulfilling the " +
+            "need for specialized education, research and training in the " +
+            "field of marketing. Presently, there are about 1200 students in " +
+            "its Bachelor, Master, M.Phil and Ph.D. programmes in the department.",
+    lat = 5.63424,
+    long = 66.921,
+    telephone = "880-2-8611996- (7954)",
+    email = "marketing@du.ac.bd"
+)
--- backend_api/src/main/kotlin/io/github/kabirnayeem99/httpapi/routes/InformationRoutes.kt
+++ backend_api/src/main/kotlin/io/github/kabirnayeem99/httpapi/routes/InformationRoutes.kt
@@ -1,19 +1,27 @@
 package io.github.kabirnayeem99.httpapi.routes
 
-import io.github.kabirnayeem99.httpapi.models.informationStorage
+import io.github.kabirnayeem99.httpapi.models.Information
+import io.github.kabirnayeem99.httpapi.models.information
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Route.informationRouteing() {
     route("/information") {
         get {
-            if (informationStorage.isNotEmpty()) {
-                call.respond(informationStorage)
+            if (information.lat != 0.0 && information.long != 0.0) {
+                call.respond(information)
             } else {
-                call.respondText("No information found\n", status = HttpStatusCode.NotFound)
+                call.respondText("No information is saved yet.\n", status = HttpStatusCode.NotFound)
             }
+        }
+
+        post {
+            val receivedInformation = call.receive<Information>()
+            information = receivedInformation
+            call.respondText("Information is saved successfully.", status = HttpStatusCode.Created)
         }
     }
 }
