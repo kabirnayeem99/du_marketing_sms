a9f6556bbcce0979d5697c92bcc8e6bf2b1a71bf
Naimul Kabir <kabirnayeem.99@gmail.com>
2022-02-08 23:04:35 +0600

    feat: work on refactoring major

---
+++ student_app/app/src/main/java/io/github/kabirnayeem99/dumarketingstudent/common/views/LoadingDialog.kt
@@ -0,0 +1,22 @@
+package io.github.kabirnayeem99.dumarketingstudent.common.views
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import io.github.kabirnayeem99.dumarketingstudent.R
+
+class LoadingDialog(context: Context) : Dialog(context) {
+    init {
+        window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        setCancelable(false)
+        setCanceledOnTouchOutside(false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_loading)
+    }
+
+}
